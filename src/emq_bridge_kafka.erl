2aa13e0423572db86cca1da9a5b4ec31f1a7f223
zhangli#2946 <294638057@qq.com>
2018-08-28 14:49:42 +0800

    这是一个 不懂 erlang 写的 提交

---
+++ src/emq_bridge_kafka.erl
@@ -0,0 +1,82 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2015-2017 Feng Lee <feng@emqtt.io>.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module(emq_bridge_kafka).
+
+-include_lib("emqttd/include/emqttd.hrl").
+
+-include_lib("emqttd/include/emqttd_protocol.hrl").
+
+-include_lib("emqttd/include/emqttd_internal.hrl").
+
+-import(string,[concat/2]).
+-import(lists,[nth/2]). 
+
+
+-export([load/1, unload/0]).
+-export([on_message_publish/2, on_message_delivered/4, on_message_acked/4]).
+
+%% Called when the plugin application start
+load(Env) ->
+	ekaf_init([Env]),
+    emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
+    emqttd:hook('message.delivered', fun ?MODULE:on_message_delivered/4, [Env]),
+    emqttd:hook('message.acked', fun ?MODULE:on_message_acked/4, [Env]).
+
+%% transform message and return
+on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env) ->
+    {ok, Message};
+
+on_message_publish(Message = #mqtt_message{pktid   = PkgId,
+                        qos     = Qos,
+                        retain  = Retain,
+                        dup     = Dup,
+                        topic   = Topic,
+                        payload = Payload
+						}, _Env) ->
+    io:format("publish ~s~n", [emqttd_message:format(Message)]),
+    Str1 = <<"{\"topic\":\"">>,
+    Str2 = <<"\", \"message\":[">>,
+    Str3 = <<"]}">>,
+    Str4 = <<Str1/binary, Topic/binary, Str2/binary, Payload/binary, Str3/binary>>,
+	{ok,ProduceTopic}= application:get_key(kafka_producer_topic),
+    ekaf:produce_async(ProduceTopic, Str4),	
+    {ok, Message}.
+
+
+on_message_delivered(ClientId, Username, Message, _Env) ->
+    io:format("delivered to client(~s/~s): ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
+    {ok, Message}.
+
+on_message_acked(ClientId, Username, Message, _Env) ->
+    io:format("client(~s/~s) acked: ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
+    {ok, Message}.
+
+ekaf_init(_Env) ->
+
+    {ok,BootstrapBroker} = application:get_key(bootstrap_broker),
+    {ok,PartitionStrategy}= application:get_key(partition_strategy),
+    application:set_env(ekaf, ekaf_partition_strategy, PartitionStrategy),
+    application:set_env(ekaf, ekaf_bootstrap_broker, BootstrapBroker),
+    {ok, _} = application:ensure_all_started(ekaf),
+    io:format("Initialized ekaf with ~p~n", [{"localhost", 9092}]).
+
+%% Called when the plugin application stop
+unload() ->
+    emqttd:unhook('message.publish', fun ?MODULE:on_message_publish/2),
+    emqttd:unhook('message.delivered', fun ?MODULE:on_message_delivered/4),
+    emqttd:unhook('message.acked', fun ?MODULE:on_message_acked/4).
+
---
+++ test/emq_bridge_kafka_SUITE.erl
@@ -0,0 +1,8 @@
+
+-module(emq_bridge_kafka_SUITE).
+
+-compile(export_all).
+
+all() -> [].
+
+groups() -> [].
