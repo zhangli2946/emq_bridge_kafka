af90d875a4044693c63a1f0c273093e5658728b1
zhangli#2946 <294638057@qq.com>
2018-09-05 15:26:53 +0800

    我瞎写的

--- src/emq_bridge_kafka.erl
+++ src/emq_bridge_kafka.erl
@@ -27,36 +27,12 @@
 
 
 -export([load/1, unload/0]).
--export([on_message_publish/2, on_message_delivered/4, on_message_acked/4]).
+-export([on_message_publish/2]).
 
 %% Called when the plugin application start
 load(Env) ->
-    io:format("Prev ekaf_init ===============================>~n"),
 	ekaf_init([Env]),
-    io:format("Post ekaf_init ===============================>~n"),
-    emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
-    emqttd:hook('message.delivered', fun ?MODULE:on_message_delivered/4, [Env]),
-    emqttd:hook('message.acked', fun ?MODULE:on_message_acked/4, [Env]).
-
-%% transform message and return
-% on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env) ->
-%     {ok, Message};
-
-% on_message_publish(Message = #mqtt_message{pktid   = PkgId,
-%                         qos     = Qos,
-%                         retain  = Retain,
-%                         dup     = Dup,
-%                         topic   = Topic,
-%                         payload = Payload
-% 						}, _Env) ->
-%     io:format("publish ~s~n", [emqttd_message:format(Message)]),
-%     Str1 = <<"{\"topic\":\"">>,
-%     Str2 = <<"\", \"message\":[">>,
-%     Str3 = <<"]}">>,
-%     Str4 = <<Str1/binary, Topic/binary, Str2/binary, Payload/binary, Str3/binary>>,
-% 	{ok,ProduceTopic}= application:get_key(kafka_producer_topic),
-%     ekaf:produce_async(ProduceTopic, Str4),	
-%     {ok, Message}.
+    emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]).
 
 get_form_clientid({ClientId, Username}) -> ClientId;
 get_form_clientid(From) -> From.
@@ -73,49 +49,50 @@ on_message_publish(Message,_Env) ->
     {ok, KTopic} = application:get_env(ekaf, ekaf_bootstrap_topics),
     ClientId = get_form_clientid(From),
     Username = get_form_username(From),
-    Str = [
-              {client_id, ClientId},
-              {message, [
-                            {username, Username},
-                            {topic, Topic},
-                            {payload, Payload},
-                            {qos, Qos},
-                            {dup, Dup},
-                            {retain, Retain}
-                        ]},
-               {cluster_node, node()},
-               {ts, emqttd_time:now_ms()}
-           ],
-    Json = mochijson2:encode(Str),
+    Json = mochijson2:encode([
+        {client_id, ClientId},
+        {message, [
+            {username, Username},
+            {topic, Topic},
+            {payload, Payload},
+            {qos, Qos},
+            {dup, Dup},
+            {retain, Retain}
+        ]},
+        {cluster_node, node()},
+        {ts, emqttd_time:now_ms()}
+    ]),
     ekaf:produce_async(KTopic, list_to_binary(Json)),
     io:format("publish ~s~n", [emqttd_message:format(Message)]),
     {ok, Message}.
 
-
-on_message_delivered(ClientId, Username, Message, _Env) ->
-    io:format("delivered to client(~s/~s): ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
-    {ok, Message}.
-
-on_message_acked(ClientId, Username, Message, _Env) ->
-    io:format("client(~s/~s) acked: ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
-    {ok, Message}.
+on_message_publish(Message{topic=<<"$SYS/", _/binary>>},_Env) ->
+        {ok, KTopic} = application:get_env(ekaf, rxtopics),
+        Topic = Message#mqtt_message.topic,
+        Payload = Message#mqtt_message.payload,
+        Json = mochijson2:encode([
+            {topic, Topic},
+            {payload, Payload},
+        ]),
+        ekaf:produce_async(KTopic, list_to_binary(Json)),
+        io:format("publish ~s~n", [emqttd_message:format(Message)]),
+        {ok, Message}.
 
 ekaf_init(_Env) ->
     {ok, Kafka_Env} = application:get_env(?MODULE, server),
     Host = proplists:get_value(host, Kafka_Env),
     Port = proplists:get_value(port, Kafka_Env),
     Topic = proplists:get_value(topic, Kafka_Env),
+    RxTopic = proplists:get_value(rxtopic, Kafka_Env),
     Broker = {Host, Port},
     application:set_env(ekaf, ekaf_partition_strategy, strict_round_robin),
     application:set_env(ekaf, ekaf_bootstrap_broker, Broker),
     application:set_env(ekaf, ekaf_buffer_ttl, 100),
     application:set_env(ekaf, ekaf_bootstrap_topics, list_to_binary(Topic)),
+    application:set_env(ekaf, rxtopics, list_to_binary(RxTopic)),
     {ok, _} = application:ensure_all_started(ekaf),
     io:format("Initialized ekaf with ~p~n", [{"localhost", 9092}]).
 
 %% Called when the plugin application stop
 unload() ->
     emqttd:unhook('message.publish', fun ?MODULE:on_message_publish/2),
-    emqttd:unhook('message.delivered', fun ?MODULE:on_message_delivered/4),
-    emqttd:unhook('message.acked', fun ?MODULE:on_message_acked/4).
-
